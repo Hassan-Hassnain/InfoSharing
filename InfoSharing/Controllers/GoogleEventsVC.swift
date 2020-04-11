8244b80f572bfd9db81aabf4420e436d1cc63602
Hassan Hassnain <ahh.devoders@gmail.com>
2020-04-12 02:03:35 +0500

    Adding and fetching event working

--- InfoSharing/Controllers/GoogleEventsVC.swift
+++ InfoSharing/Controllers/GoogleEventsVC.swift
@@ -14,13 +14,25 @@ import UIAlertDateTimePicker
 
 class GoogleEventsVC: GoogleCalendarEventService {
     
+    var datePicker: UIAlertDateTimePicker?
+    var myStartDate = ""
+    var myEndDate = ""
+    var isStartDate = false
+    var startDate: Date?
+    var endDate: Date?
+    
+    var events: [CalendarEvent] = []
     
     @IBOutlet weak var titleTF: UITextField!
     @IBOutlet weak var descriptionTF: UITextField!
-    @IBOutlet weak var userMsg: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        delegate = self
+        
     }
     @IBAction func signInWithGoogleButton(_ sender: Any) {
         GIDSignIn.sharedInstance()?.signIn()
@@ -33,17 +45,71 @@ class GoogleEventsVC: GoogleCalendarEventService {
     
     @IBAction func endDateButton(_ sender: Any) {
         
-       showDatePicker()
+        showDatePicker()
     }
     @IBAction func addEventButton(_ sender: Any) {
+        guard let starTime = startDate,let endTime = endDate  else {
+            print("please provide both dates")
+            return
+        }
         
-//        addEventoToGoogleCalendar(summary: titleTF.text!, description: descriptionTF.text!, startTime: myStartDate, endTime:myEndDate)
+        addEventoToGoogleCalendar(summary: titleTF.text!, description: descriptionTF.text!, startTime: starTime, endTime:endTime)
         
-//        fetchEvents()
-        getEvent(startDate: startDate!, endDate: endDate!)
-        
-        userMsg.text = outPut
-        print(outPut)
     }
+    
+    @IBAction func fetchEventButton(_ sender: Any) {
+        getEvents(startDate: startDate!, endDate: endDate!)
+    }
+}
+extension GoogleEventsVC: GoogleCalendarEventServiceDelegate {
+    func didfetchResultUpdated(events: [CalendarEvent]) {
+        self.events = events
+        tableView.reloadData()
+    }
+}
+
+extension GoogleEventsVC: UIAlertDateTimePickerDelegate {
+    func positiveButtonClicked(withDate date: Date) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        
+        if isStartDate {
+            myStartDate = dateFormatter.string(from: date)
+            startDate = date
+            isStartDate = false
+        } else {
+            endDate = date
+            self.myEndDate = dateFormatter.string(from: date)
+        }
+    }
+    
+    func showDatePicker(){
+        datePicker = UIAlertDateTimePicker(withPickerMode: .date, pickerTitle: "Select Date", showPickerOn: self.view)
+        datePicker?.delegate = self
+        datePicker?.showAlert()
+    }
+}
+
+extension GoogleEventsVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TVCell") as! TVEvent
+        cell.summery.text = calenderEvent[indexPath.row].Description
+        cell.startTime.text = calenderEvent[indexPath.row].start
+        cell.endTime.text = calenderEvent[indexPath.row].end
+        return cell
+    }
+    
+    
+}
+
+class TVEvent: UITableViewCell {
+    @IBOutlet weak var startTime: UILabel!
+    @IBOutlet weak var endTime: UILabel!
+    @IBOutlet weak var summery: UILabel!
     
 }
--- InfoSharing/Services/GoogleCalendarEventService.swift
+++ InfoSharing/Services/GoogleCalendarEventService.swift
@@ -10,22 +10,24 @@ import Foundation
 import GoogleAPIClientForREST
 import GoogleSignIn
 import GTMSessionFetcher
-import UIAlertDateTimePicker
+//import UIAlertDateTimePicker
 
 let CLIENT_ID = "750374453418-363del5odp0q8a5f5oggsmqg78e0519i.apps.googleusercontent.com"
 
+protocol GoogleCalendarEventServiceDelegate {
+    func didfetchResultUpdated(events: [CalendarEvent]) -> Void
+}
 
 class GoogleCalendarEventService: UIViewController {
     
-    var outPut = ""
-    var startDate: Date?
-    var endDate: Date?
+    var delegate: GoogleCalendarEventServiceDelegate?
+    
+    var calenderEvent: [CalendarEvent] = [] { didSet { delegate?.didfetchResultUpdated(events: calenderEvent)}  }
     
     let scopes = [kGTLRAuthScopeCalendar]
-//    let service = GTLRCalendarService()
     
     /// Creates calendar service with current authentication
-     lazy var calendarService: GTLRCalendarService? = {
+    lazy var calendarService: GTLRCalendarService? = {
         let service = GTLRCalendarService()
         // Have the service object set tickets to fetch consecutive pages
         // of the feed so we do not need to manually fetch them
@@ -44,11 +46,6 @@ class GoogleCalendarEventService: UIViewController {
         return service
     }()
     
-    var datePicker: UIAlertDateTimePicker?
-    var myStartDate = ""
-    var myEndDate = ""
-    var isStartDate = false
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,11 +53,10 @@ class GoogleCalendarEventService: UIViewController {
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().scopes = scopes
         GIDSignIn.sharedInstance()?.presentingViewController = self
-        
-        
+         
     }
     
-    func addEventoToGoogleCalendar(summary : String, description :String, startTime : String, endTime : String) {
+    func addEventoToGoogleCalendar(summary : String, description :String, startTime : Date, endTime : Date) {
         let calendarEvent = GTLRCalendar_Event()
         
         calendarEvent.summary = "\(summary)"
@@ -68,17 +64,10 @@ class GoogleCalendarEventService: UIViewController {
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
-        let startDate = dateFormatter.date(from: startTime)
-        let endDate = dateFormatter.date(from: endTime)
         
-        guard let toBuildDateStart = startDate else {
-            print("Error getting start date")
-            return
-        }
-        guard let toBuildDateEnd = endDate else {
-            print("Error getting end date")
-            return
-        }
+        let toBuildDateStart = startTime
+        let toBuildDateEnd = endTime
+        
         calendarEvent.start = buildDate(date: toBuildDateStart)
         calendarEvent.end = buildDate(date: toBuildDateEnd)
         
@@ -91,59 +80,21 @@ class GoogleCalendarEventService: UIViewController {
                 print(error?.localizedDescription ?? "")
             }
         }
-        
-        getEvents()
     }
     
     // you will probably want to add a completion handler here
-    func getEvents(for calendarId: String = "primary") {
-        guard let service = self.calendarService else {
-            return
-        }
-
-        // You can pass start and end dates with function parameters
-        let startDateTime = GTLRDateTime(date: Calendar.current.startOfDay(for: Date()))
-        let endDateTime = GTLRDateTime(date: Date().addingTimeInterval(60*60*24))
-        
-//        let query = GTLRCalendarQuery_EventsList.query(withCalendarId: "primary")
-        let eventsListQuery = GTLRCalendarQuery_EventsList.query(withCalendarId: "primary")
-//        let eventsListQuery = GTLRCalendarQuery_EventsList.query(withCalendarId: calendarId)     //orignal line
-        eventsListQuery.timeMin = startDateTime
-        eventsListQuery.timeMax = endDateTime
-
-        calendarService?.executeQuery(eventsListQuery, delegate: self, didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
-        
-//        calendarService?.executeQuery(, completionHandler: <#T##GTLRServiceCompletionHandler?##GTLRServiceCompletionHandler?##(GTLRServiceTicket, Any?, Error?) -> Void#>)
-        _ = service.executeQuery(eventsListQuery, completionHandler: { (ticket, result, error) in
-            print(error as Any)
-            print(ticket as Any)
-            print(result as Any)
-            guard error == nil, let items = (result as? GTLRCalendar_Events)?.items else {
-                return
-            }
-            
-            if items.count > 0 {
-                print(items)
-                // Do stuff with your events
-            } else {
-                // No events
-            }
-        })
-    }
-    func getEvent(startDate: Date, endDate: Date) {
+    func getEvents(startDate: Date, endDate: Date) {
         let query = GTLRCalendarQuery_EventsList.query(withCalendarId: "primary")
-            query.maxResults = 10
-            query.timeMin = GTLRDateTime(date: startDate)
+        query.maxResults = 10
+        query.timeMin = GTLRDateTime(date: startDate)
         query.timeMax = GTLRDateTime(date: endDate)
-            query.singleEvents = true
-            query.orderBy = kGTLRCalendarOrderByStartTime
-            calendarService?.executeQuery(
-                query,
-                delegate: self,
-                didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
-        }
-
-    
+        query.singleEvents = true
+        query.orderBy = kGTLRCalendarOrderByStartTime
+        calendarService?.executeQuery(
+            query,
+            delegate: self,
+            didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
+    }
     
     // Helper to build date
     func buildDate(date: Date) -> GTLRCalendar_EventDateTime {
@@ -153,72 +104,27 @@ class GoogleCalendarEventService: UIViewController {
         return dateObject
     }
     
-}
-
-extension GoogleCalendarEventService: GIDSignInDelegate{
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-       
-    }
-    
-    
-}
-extension GoogleCalendarEventService: UIAlertDateTimePickerDelegate {
-    func positiveButtonClicked(withDate date: Date) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
-        if isStartDate {
-            myStartDate = dateFormatter.string(from: date)
-            startDate = date
-            isStartDate = false
-        } else {
-            endDate = date
-            self.myEndDate = dateFormatter.string(from: date)
-        }
-    }
-    
-    func showDatePicker(){
-        datePicker = UIAlertDateTimePicker(withPickerMode: .date, pickerTitle: "Select Date", showPickerOn: self.view)
-        datePicker?.delegate = self
-        datePicker?.showAlert()
-    }
-}
-
-extension GoogleCalendarEventService {
-    // Construct a query and get a list of upcoming events from the user calendar
-    func fetchEvents() {
-        let query = GTLRCalendarQuery_EventsList.query(withCalendarId: "primary")
-        query.maxResults = 10
-        query.timeMin = GTLRDateTime(date: Date())
-        query.singleEvents = true
-        query.orderBy = kGTLRCalendarOrderByStartTime
-        calendarService?.executeQuery(
-            query,
-            delegate: self,
-            didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
-    }
-
     // Display the start dates and event summaries in the UITextView
     @objc func displayResultWithTicket(ticket: GTLRServiceTicket, finishedWithObject response : GTLRCalendar_Events, error : NSError?) {
-
         if let error = error {
             showAlert(title: "Error", message: error.localizedDescription)
             return
         }
-
-        var outputText = ""
+        calenderEvent = []
         if let events = response.items, !events.isEmpty {
             for event in events {
                 let start = event.start!.dateTime ?? event.start!.date!
-                let startString = DateFormatter.localizedString(
-                    from: start.date,
-                    dateStyle: .short,
-                    timeStyle: .short)
-                outputText += "\(startString) - \(event.summary!)\n"
+                let startString = DateFormatter.localizedString( from: start.date, dateStyle: .short, timeStyle: .short)
+                let end = event.end!.dateTime ?? event.end!.date!
+                let endString = DateFormatter.localizedString( from: end.date, dateStyle: .short, timeStyle: .short)
+                let summery = event.summary ?? ""
+                
+                let evnt = CalendarEvent(Description: summery, start: startString, end: endString)
+                calenderEvent.append(evnt)
             }
         } else {
-            outputText = "No upcoming events found."
+            calenderEvent = []
         }
-        outPut = outputText
     }
     
     // Helper for showing an alert
@@ -236,5 +142,17 @@ extension GoogleCalendarEventService {
         alert.addAction(ok)
         present(alert, animated: true, completion: nil)
     }
+}
 
+extension GoogleCalendarEventService: GIDSignInDelegate{
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+    }
+}
+
+struct CalendarEvent {
+    //    var title: String
+    var Description: String
+    var start: String
+    var end: String
 }
